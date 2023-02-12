f7a9b1484231bcea20c666772975f63ea0e51ba6
sabrina <Sabrina.Rodriguez@jala.university>
2023-02-11 22:09:49 -0300

    Primeras modificaciones y creacion de proyecto Pokedex

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const Url = "https://pokeapi.co/api/v2/pokemon/";
+  const [pokemon, setPokemon] = useState({});
+  const [currentId , setCurrentId] = useState(1);
+
+  const getPokemon = (id) => {
+    setCurrentId(id);
+  }
+
+  useEffect(() => {
+    fetch(Url + currentId)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCurrentId(data.id);
+        setPokemon(data);
+      });
+  }, [currentId]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <label>{pokemon.name}</label>
+        </div>
+        <div>
+          <img src={pokemon.sprites?.front_default} alt="pokemon" />
+        </div>
+        <div>
+            <button onClick={() => getPokemon(currentId + 1)}>Next</button>
+        </div>
+        <div>
+          <label> 
+            Weight: {pokemon.weight}
+          </label>
+        </div>
+        <div>
+          Abilities: 
+          {pokemon.abilities.map((ability) => {
+            return (
+              <div key={ability.ability.name}>
+                {ability.ability.name}
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
