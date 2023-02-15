e686a4f9d16cac50b9afe54526a1e04f339c962b
sabrina <Sabrina.Rodriguez@jala.university>
2023-02-15 20:02:23 -0300

    weaksseas

--- src/Components/card.js
+++ src/Components/card.js
@@ -24,7 +24,7 @@ function Card() {
         .then( types => {
             const weaknesses1 = types.map( type => type.damage_relations.double_damage_from.map( item => item.name ) );
             const weaknesses2 = types.map( type => type.damage_relations.half_damage_from.map( item => item.name ) );
-            const dobleDamage = weaknesses1.flat(); // flat() es un metodo de los arrays que convierte un array de arrays en un array plano
+            const dobleDamage = weaknesses1.flat(); // flat() es un metodo de los arrays que convierte un array de arrays en un array plano // [ [1,2,3], [4,5,6] ] => [1,2,3,4,5,6]
             const halfDamage = weaknesses2.flat();
             const weaknesses = dobleDamage.filter( item => !halfDamage.includes(item) ); // filter() es un metodo de los arrays que filtra los elementos de un array segun una condicion. En este caso filtra los elementos que no estan en el array halfDamage
             console.log(weaknesses);
